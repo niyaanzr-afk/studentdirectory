4ee8beb14f569114cefe87b96cecf5a1e9b2ef8f
niyaanzr-afk <niyaanzr@gmail.com>
2026-06-15 12:47:50 +0530

    student directory finalised

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,8 @@ import 'package:studentdirectory/student.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async{await Firebase.initializeApp(
-  
-    options: DefaultFirebaseOptions.currentPlatform,
-);
-  runApp(MaterialApp(debugShowCheckedModeBanner: false,home:Student(),));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Student()));
 }
